4b5d0dde19d0d132bd2677b50b88f8bc26ef0c9e
MatheusMedeiroscl <134938644+mmamedeiros@users.noreply.github.com>
2023-11-08 13:40:25 -0300

    Adicionando meus primeiros projetos
    1° (Web 2.1) - referente ao projeto da faculdade
    2° (Apps) - primeiros sites funcionais usando ferramentas front-end

---
+++ apps/Questionário/questions.js
@@ -0,0 +1,38 @@
+export default[ // Banco de dados com questões
+
+    { // cada chave dessa representa o obj de cada pergunta
+        question: "Quanto é 2+2?",
+        ansewrs: [ // array das respostas
+            {option: "4", correct: true}, // obj pra cada resposta
+            {option: "2", correct: false},
+            {option: "10", correct: false}
+        ]
+    },
+
+    { 
+        question: "Quanto é 100 - 10?",
+        ansewrs: [ 
+            {option: "4", correct: false}, 
+            {option: "90", correct: true},
+            {option: "100", correct: false}
+        ]
+    },
+    
+    { 
+        question: "Quanto é 100 /2?",
+        ansewrs: [ 
+            {option: "25", correct: false}, 
+            {option: "50", correct: true},
+            {option: "500", correct: false}
+        ]
+    },
+    
+    { 
+        question: "Quanto é -1 + 2?",
+        ansewrs: [ 
+            {option: "-1", correct: false}, 
+            {option: "3", correct: false},
+            {option: "1", correct: true}
+        ]
+    }
+]
