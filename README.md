e3d4f53993670393d8fbb10963dea19c73a9df8e
MatheusMedeiroscl <134938644+mmamedeiros@users.noreply.github.com>
2023-11-08 14:26:09 -0300

    Arrumando os Bugs do Questionário
    Erros bobos de escrita

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Meus-Projetos
  Colocando todos os meus projetos atuais
+
+ 11/08/2023 
+    - Aproveitando os conhecimentos de como trabalhar com os meus repositóros, coloquei meus principais projetos aqui para poder modifica-los na faculdade e no trabalho 
+    - Fiz o questinário funcionar perfeitamente:
+        - todos os erros foram na escrita, errava na hora de escrever os nomes de variaveis ou te comandos mesmo
+        - se atentar a esses dados
+        -codar e falar oque está fazendo para não errar mais nisso 
+
--- apps/Questionário/script.js
+++ apps/Questionário/script.js
@@ -12,8 +12,8 @@ let currentIndex = 0; // armazena o index da questão atual
 let questionsCorrect = 0; //armazena a quantidade de acertos
 
 btnRestart.onclick = () =>{
-    content.computedStyleMap.display = "flex";
-    contentFinish.computedStyleMap.display = "none";
+    content.style.display = "flex";
+    contentFinish.style.display = "none";
 
     currentIndex = 0;
      questionsCorrect = 0; 
@@ -21,9 +21,10 @@ btnRestart.onclick = () =>{
 }
 
 function nextQuestion(e){
-    if(e.target.getAttribute("data-correct") === "true"){
-        questionsCorrect ++; // verifica se você acertou a questãoi  e caso tenha acertado ele atribui um ponto
-    }
+    if (e.target.getAttribute("data-correct") === "true") {
+        questionsCorrect++;
+      } // verifica se você acertou a questãoi  e caso tenha acertado ele atribui um ponto
+    
 
     if(currentIndex < questions.length - 1){ // verifica se há mais questões paara serem feitas
         currentIndex++; 
@@ -34,11 +35,11 @@ function nextQuestion(e){
 
 };
 
-function finish(){{
+function finish(){
     textFinish.innerHTML = `você acertou ${questionsCorrect} de ${questions.length}`;
-    content.computedStyleMap.display = "none";
-    contentFinish.computedStyleMap.display = "flex";
-}}
+    content.style.display = "none";
+    contentFinish.style.display = "flex";
+}
 
 function loadQuestion(){ // funçaõ rresponsavel por rodar as questões
     spnQtd.innerHTML = `${currentIndex + 1}/${questions.length}`; // mostra a questão que você está e quantas questões o programa possui
@@ -46,12 +47,13 @@ function loadQuestion(){ // funçaõ rresponsavel por rodar as questões
     answers.innerHTML = ""; // garante que ansewres fique vazia
     question.innerHTML = item.question; // mostra a questão do item atual
 
-    item.ansewrs.forEach((answer) =>{
+    item.answers.forEach((answer) =>{
         const div = document.createElement("div"); // cria um elemento div no html
 
         div.innerHTML = `
-        <button class="answer" data-current="${answer.correct}">
+        <button class="answer" data-correct="${answer.correct}">
         ${answer.option} </button>`; // alimenta o elemento criado com as opções de resposta
+    
         answers.appendChild(div);
     });
 
